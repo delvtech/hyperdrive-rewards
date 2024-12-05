6a95a1e2943074217a0a74c24ac8246e08581599
Matthew Brown <mattfdbrown@gmail.com>
2024-12-05 13:32:23 -0600

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 import https from "https";
 import fs from "fs";
 
+// Instantiate the app.
 const app = express();
+
+// Enable CORS.
+app.use(cors());
+
 const port = 443;
 
-// Load SSL Certificates
+// Load SSL Certificates.
 const sslOptions = {
   key: fs.readFileSync(
     "/etc/letsencrypt/live/rewards.hyperdrive.money/privkey.pem"
@@ -16,13 +22,13 @@ const sslOptions = {
   ),
 };
 
-// Define your routes
+// Main rewards endpoint.
 app.get("/get/rewards/:address", (req: Request, res: Response) => {
   const { address } = req.params;
   res.json({ message: `Rewards for address: ${address}` });
 });
 
-// Start HTTPS server
+// Start HTTPS server.
 https.createServer(sslOptions, app).listen(port, () => {
   console.log(`Server running on https://localhost:${port}`);
 });
