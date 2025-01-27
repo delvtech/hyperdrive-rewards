3ee10dc503acec1385de332b8ca960db0293da37
Ubuntu <ubuntu@ip-172-31-15-65.us-east-2.compute.internal>
2025-01-27 16:37:02 +0000

    fund merkle contract

---
+++ script/FundHyperdriveRewards.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import "src/contracts/HyperdriveRewards.sol";
+import { Merkle } from "universal-rewards-distributor/lib/murky/src/Merkle.sol";
+import { console2 as console } from "forge-std/console2.sol";
+
+
+contract FundHyperdriveRewards is Script {
+    struct MerkleEntry {
+        string claimable; // Keep claimable as a string in JSON
+        address token;
+        address user;
+    }
+
+    struct RewardData {
+        address user;
+        uint256 chainId;
+        address claimContract;
+        string claimableAmount;
+        address rewardToken;
+        bytes32[] merkleProof;
+        uint256 merkleProofLastUpdated;
+    }
+
+    function run() external {
+
+        // Read environment variables
+        address rewardsContract = vm.envAddress("REWARDS_CONTRACT");
+        uint256 rewardsAmount = vm.envUint("REWARDS_AMOUNT");
+        IERC20 token = IERC20(vm.envAddress("REWARDS_TOKEN"));
+        address tokenWhale = vm.envAddress("REWARDS_WHALE");
+
+        uint256 amount = rewardsAmount * 1 ether;
+        console.log('rewardsContract', rewardsContract);
+        console.log('amount', amount);
+        console.log('tokenWhale', tokenWhale);
+
+        vm.startBroadcast(tokenWhale);
+        vm.deal(tokenWhale, 1 ether);
+        token.approve(rewardsContract, amount);
+        token.transfer(rewardsContract, amount);
+
+        vm.stopBroadcast();
+    }
+}
